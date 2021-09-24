c072089bc77c9cf2aaac56a526cdbe26ae2459c1
willisthewizard <wyattmackubbin@gmail.com>
2021-09-23 19:21:09 -0600

    now with multiple spinning cubes

--- src/Renderer/Renderer.cpp
+++ src/Renderer/Renderer.cpp
@@ -73,6 +73,20 @@ int main()
         0,1,3,
         1,2,3
     };
+
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+
+    };
     //initizlize GLFW
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -255,7 +269,7 @@ int main()
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
 
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));  
+        //model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));  
 
         view = glm::translate(view, glm::vec3(0.0f,0.0f, -3.0f));
         //glm::mat4 projection;
@@ -268,14 +282,26 @@ int main()
         glUniformMatrix4fv(viewLoc,1,GL_FALSE,&view[0][0]);
 
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"projection"), 1, GL_FALSE, &projection[0][0]);
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"view"), 1, GL_FALSE, &view[0][0]);
+    
         
 
         //unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
         //glUniformMatrix4fv(transformLoc,1,GL_FALSE,glm::value_ptr(transform));
         glBindVertexArray(VAO);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i +1;
+            model = model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0f));  
+            glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"model"), 1, GL_FALSE, &model[0][0]);
 
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
  
-        glDrawArrays(GL_TRIANGLES,0,36);
+        //glDrawArrays(GL_TRIANGLES,0,36);
         //glBindVertexArray(0);
 
         glfwSwapBuffers(window);
